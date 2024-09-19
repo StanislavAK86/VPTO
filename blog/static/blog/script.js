8feeaa785ce79d630594ebe931af7f863d8b7fa6
StanislavAK <kirkizhsa@mail.ru>
2024-09-19 07:11:49 +0500

    Изменен слайдер

--- blog/static/blog/script.js
+++ blog/static/blog/script.js
@@ -1,19 +1,24 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function() { // связь с DOM
   const sliders = document.querySelectorAll('.slider'); // выбираем все слайдеры
 
-  sliders.forEach(slider => { // для каждого слайдера
-      let currentIndex = 0;
-      const slides = slider.querySelectorAll('.slide');
+  sliders.forEach(slider => { // для каждого слайдера 
+      let currentIndex = 0; // текущий слайд по индексу
+      const slides = slider.querySelectorAll('.slide'); // выбираем все слайды
 
       function showSlide(index) { // показать слайд по индексу
-          slides.forEach((slide, i) => {
-              slide.style.display = i === index ? 'block' : 'none';
+          slides.forEach((slide, i) => { 
+              slide.style.display = i === index ? 'block' : 'none'; // показать слайд по индексу 
           });
       }
 
       function nextSlide() { // следующий слайд по индексу 
-          currentIndex = (currentIndex + 1) % slides.length;
-          showSlide(currentIndex);
+          currentIndex = (currentIndex + 1) % slides.length; // индекс следующего слайда
+          showSlide(currentIndex); // показать следующий слайд
+
+
       }
 
       setInterval(nextSlide, 3000); // следующий слайд каждые 3 секунды
